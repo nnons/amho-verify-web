05385a2651acdd265022871a21f6c7a7e7fbd0c4
nnon <nn0n@duck.com>
2022-11-12 23:13:09 -0500

    Updated index dir in backend

--- backend/app.js
+++ backend/app.js
@@ -23,8 +23,8 @@ app.use(
   })
 )
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
